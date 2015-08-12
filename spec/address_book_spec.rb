00a9b68ab85c148cff7abe1494fc6006bc30cc19
gadgetpink <diane.kulseth329@gmail.com>
2015-08-12 10:07:16 -0500

    Assignment 21 completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,4 +36,14 @@ RSpec.describe AddressBook do
       expect(new_entry.email).to eq 'augusta.king@lovelace.com'
     end
   end
+
+  context ".remove_entry" do
+    it "removes a single entry from the address book" do
+    book = AddressBook.new
+    entry = book.entries[0]
+    book.remove_entry(entry)
+
+    expect(book.entries.size).to eq 0
+  end
+end
 end
